f8a4cff3cd8d0edfd19ad8b97b3bedf7c79bc0c3
SJ Brat <justanobser@gmail.com>
2019-05-14 19:36:50 -0400

    updates/ adding masonry

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-    console.log( "ready!" );
-
-
-  $( "#bio-grid-item" ).hide();
-    console.log('hide me')
-
-  
-});
+$(function() {
+  $(".container-grid").masonry({
+    itemSelector: '.grid-item',
+    columnWidth: 100
+  });
+})
+  //   console.log( "ready!" );
+  //
+  //
+  // $( "#bio-grid-item" ).hide();
+  //   console.log('hide me')
